f02c49e48bf7bb732b75560ab7ae293b4af23fc1
suresh <sk>
2026-09-21 19:03:07 +0530

    main js

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/sparsha-llp-app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
